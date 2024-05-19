4b5dc2b2874bdf77344548db8a45250672431faf
letsgitcracking <45171676+letsgitcracking@users.noreply.github.com>
2024-05-19 18:22:22 +0100

    Added arguments in filename to ensure no clash

--- script.sh
+++ script.sh
@@ -47,20 +47,21 @@ fi
 REPO_PATH=$(pwd)
 
 # Name of the file to be modified
-FILENAME="git-fill.txt"
+FILENAME="git-fill-$START_DATE-$END_DATE.txt"
 
 # Generate dates from START_DATE to END_DATE
 d=$(date -I -d "$START_DATE") || exit -1
 END_DATE=$(date -I -d "$END_DATE + 1 day") # Adjust end date to be inclusive
-while [ "$d" != "$END_DATE" ]; do 
+
+while [ "$d" != "$END_DATE" ]; do
     # Get the day of the week (1 for Sunday, 7 for Saturday)
     day_of_week=$(date -d "$d" +%u)
     day_of_week=$((day_of_week % 7 + 1)) # Adjust to GitHub's week representation
 
     if ((day_of_week > 1)) && ((day_of_week < 7)); then
-        commits=$(( RANDOM % (WEEKDAY_COMMIT_MAX - WEEKDAY_COMMIT_MIN + 1 ) + WEEKDAY_COMMIT_MIN ))
+        commits=$((RANDOM % (WEEKDAY_COMMIT_MAX - WEEKDAY_COMMIT_MIN + 1) + WEEKDAY_COMMIT_MIN))
     else
-        commits=$(( RANDOM % (WEEKEND_COMMIT_MAX - WEEKEND_COMMIT_MIN + 1 ) + WEEKEND_COMMIT_MIN ))
+        commits=$((RANDOM % (WEEKEND_COMMIT_MAX - WEEKEND_COMMIT_MIN + 1) + WEEKEND_COMMIT_MIN))
     fi
 
     for ((i=1;i<=commits;i++)); do
@@ -72,6 +73,6 @@ while [ "$d" != "$END_DATE" ]; do
         export GIT_COMMITTER_DATE="$(date --rfc-2822 -d "$d")"
         git commit --date "$(date --rfc-2822 -d "$d")" -m "Commit $i for $d"
     done
-  
+
     d=$(date -I -d "$d + 1 day")
 done
